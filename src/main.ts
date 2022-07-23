852c4bc4e89163d4608844d94710fdbf22334aa1
cholis04 <korunkolis04@gmail.com>
2022-07-23 12:11:07 +0700

    Add Fungtionality Responsive Menu

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
-console.log('Hello world!');
+const toggleButtonMenu = document.getElementById(
+  'toggle-button-menu'
+) as HTMLButtonElement;
+const navbarOverlay = document.getElementById(
+  'navbar-overlay'
+) as HTMLDivElement;
+const navbarMenulist = document.getElementById(
+  'navbar-menulist'
+) as HTMLUListElement;
+
+const tabletBP = 768;
+let menuIsOpen = false;
+
+// Toggle Menu on Mobile
+const toggleMenu = () => {
+  navbarOverlay.style.display = menuIsOpen ? 'block' : 'none';
+  navbarMenulist.style.display = menuIsOpen ? 'block' : 'none';
+
+  toggleButtonMenu.setAttribute('aria-expanded', menuIsOpen.toString());
+  toggleButtonMenu.className = menuIsOpen
+    ? 'navbar__toggleMenu--close'
+    : 'navbar__toggleMenu--open';
+};
+
+// On load
+window.onload = () => {
+  //   When toggle button menu is clicked
+  toggleButtonMenu?.addEventListener('click', () => {
+    const windowWidth = window.innerWidth;
+
+    // Check screen size
+    if (windowWidth < tabletBP) {
+      menuIsOpen = !menuIsOpen;
+      toggleMenu();
+    }
+  });
+};
